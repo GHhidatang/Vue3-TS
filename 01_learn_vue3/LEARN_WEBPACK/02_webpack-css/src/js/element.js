9178cc3dba29f703ae94afcf1ba9d2b48b3452b7
GHhidatang <wai2333@163.com>
2022-03-01 22:35:40 +0800

    解决了昨天的问题 以及学习loader的使用方式 和一些工具

--- 01_learn_vue3/LEARN_WEBPACK/02_webpack-css/src/js/element.js
+++ 01_learn_vue3/LEARN_WEBPACK/02_webpack-css/src/js/element.js
@@ -1,4 +1,6 @@
 import "../css/style.css";
+import "../css/title.less";
+
 const divEl = document.createElement("div");
 divEl.className = "title";
 divEl.innerHTML = "你好,大唐";
--- 01_learn_vue3/LEARN_WEBPACK/02_webpack-css/webpack.config.js
+++ 01_learn_vue3/LEARN_WEBPACK/02_webpack-css/webpack.config.js
@@ -5,14 +5,29 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./build"),
         filename: "bundle.js"
-    } // ,
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.css$/,
-    //             loader: "css-loader" // 这个是loader的语法糖
-    //         }
-    //     ]
-    // }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                // loader: "css-loader" // 这个是loader的语法糖
+
+                // 当运行use里面的内容时是按照从上往下的方式运行的
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader"
+                ]
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
+            }
+        ]
+    }
 }
 
