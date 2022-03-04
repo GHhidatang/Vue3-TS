a343444d1dc1d1cb03fc6df8e445b03d7e8e30f0
GHhidatang <wai2333@163.com>
2022-03-04 23:40:54 +0800

    图片的载入

---
+++ 01_learn_vue3/LEARN_WEBPACK/03_webpack-资源/src/js/element.js
@@ -0,0 +1,9 @@
+const divEl = document.createElement("div");
+divEl.className = "title";
+divEl.innerHTML = "你好,大唐";
+
+const bgDivEl = document.createElement('div');
+bgDivEl.className = "image-bg";
+
+document.body.appendChild(divEl);
+document.body.appendChild(bgDivEl);
