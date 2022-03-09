4932fa003f286f11b0ecae02b90421574642764b
GHhidatang <wai2333@163.com>
2022-03-09 23:14:33 +0800

    加载图片的另一种方法

--- 01_learn_vue3/LEARN_WEBPACK/03_webpack-资源/src/js/element.js
+++ 01_learn_vue3/LEARN_WEBPACK/03_webpack-资源/src/js/element.js
@@ -1,5 +1,7 @@
 import "../css/image.css";
 
+import zzhuImage from '../img/1111.jpg';
+
 const divEl = document.createElement("div");
 divEl.className = "title";
 divEl.innerHTML = "你好,大唐";
@@ -7,5 +9,9 @@ divEl.innerHTML = "你好,大唐";
 const bgDivEl = document.createElement('div');
 bgDivEl.className = "image-bg";
 
+const imgEl = document.createElement("img");
+imgEl.src = zzhuImage;
+
 document.body.appendChild(bgDivEl);
 document.body.appendChild(divEl);
+document.body.appendChild(imgEl);
